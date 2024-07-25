02bd4147d6fc629a549263e09e544dd096e915e1
rediet <redietkfle@gmail.com>
2024-07-25 22:51:42 +0300

    Getting famialiar with Scopes and Closures

---
+++ 08-aa-scopes-and-closures/problems/17-arrow-reverse.js
@@ -0,0 +1,29 @@
+/***********************************************************************
+Write a function reverseStr(str) that accepts a string and returns that string
+reversed. 
+
+Write this function using a fat arrow function!
+
+Examples:
+let result1 = reverseStr("hello"); // returns "olleh"
+let result2 = reverseStr("garden"); // returns "nedrag"
+let result3 = reverseStr("potato"); // returns "otatop"
+
+
+***********************************************************************/
+
+const reverseStr = (string) => {
+  let newString = '';
+  for (i = string.length - 1; i >= 0; i--) {
+    newString += string[i]
+  }
+  return newString
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+
+try {
+    module.exports = reverseStr;
+  } catch (e) {
+    return null;
+  }
---
+++ 08-aa-scopes-and-closures/problems/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let table2 = recVolume(3); // returns a function
+table2(2); // returns a function
+console.log(table2(1)); // prints 6
+console.log(table2(75)); // STILL prints 6
