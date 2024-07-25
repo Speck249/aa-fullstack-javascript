54db242c2c19eccf2409772cdfb513110fa5863f
rediet <redietkfle@gmail.com>
2024-07-25 22:52:37 +0300

    CommonJS vs ES6

---
+++ 11-aa-commonjs/index.js
@@ -0,0 +1,15 @@
+const sayHelloTo = require('./send-messages/say-hello-to');
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato')
+const { message1, message2, message3 } = require('./messages');
+
+let msg1 = message1;
+let msg2 = message2;
+let msg3 = message3;
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+sayHelloTo("Mr. Potato");
+giveMessageToMrsPotato(msg1);
+giveMessageToMrsPotato(msg2);
+giveMessageToMrsPotato(msg3);
